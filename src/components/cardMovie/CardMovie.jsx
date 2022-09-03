3f64f957e3c90fa4232a065db780123688c26e39
PauloPalacio <paulogpcoelho@gmail.com>
2022-09-03 13:41:53 -0300

    refactor card movie

--- src/components/cardMovie/CardMovie.jsx
+++ src/components/cardMovie/CardMovie.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./CardMovie.css";
 
@@ -7,31 +8,31 @@ export default function CardMovie({ movie }) {
   const imgUrl = "https://www.themoviedb.org/t/p/w200/"
   
   return (
-    <div className="card-movie">
+    <container className="card-movie">
         <span className="material-icons fav-btn">favorite</span>
-        <div 
+        <header 
         className="poster-image"
-        style={ {backgroundImage: `url(${imgUrl}${movie.backdrop_path})`}}
-        ></div>
+        style={ {backgroundImage: `url(${imgUrl}${movie.poster_path})`}}
+        ></header>
 
-        <div className="card-details">
+        <main className="card-details">
           <div className="name-movie">
-            <h4>{movie.title}</h4>
+            <h4>{movie.title}</h4> 
           </div>
-          <div className="score">
-            <span className="material-icons">star</span>
-            {movie.vote_average}
-           
+          <div className="details">
+            <div className="score">
+              <span className="material-icons">star</span>
+              {movie.vote_average}
+            </div>
+            <div className="price-movie"> 
+              <h3>R$70,00</h3>
+            </div>
           </div>
-          <div className="price-movie"> 
-            <h3>R$70,00</h3>
-          </div>
-           
-        </div>
-        <a href="#" className="btn-details">
-            Comprar
+        </main>
+        <button to="/search" className="btn-buy">
+            Adicionar
             <span className="material-icons">shopping_cart</span>
-        </a>
-    </div>
+        </button>
+    </container>
   );
 }
